2e86a98050e088fef37048a95c54c56fc0652725
Anna Bondar <bonterebon@users.noreply.github.com>
2022-02-07 13:02:22 +0200

    Added .runsettings files

--- Steps/SearchSteps.cs
+++ Steps/SearchSteps.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using TechTalk.SpecFlow;
 
 namespace EtsyBDD.Steps
@@ -6,20 +7,30 @@ namespace EtsyBDD.Steps
     [Binding]
     public class SearchSteps
     {
+        public TestContext TestContext { get; set; }
+        string url;
+        string username;
+        string password;
+
         [Given(@"search query is ""(.*)""")]
-        public void GivenSearchQueryIs(string p0)
+        public void GivenSearchQueryIs(string searchQuery)
         {
-            
+            url = TestContext.Parameters["url"] ?? "null";
+            Console.WriteLine("search query is " + searchQuery + "and url is " + url);
         }
         
         [When(@"search is run")]
         public void WhenSearchIsRun()
         {
+            username = TestContext.Parameters["username"] ?? "null";
+            Console.WriteLine("search is run and username is " + username);
         }
         
         [Then(@"every item in search results has ""(.*)"" in title")]
         public void ThenEveryItemInSearchResultsHasInTitle(string p0)
         {
+            password = TestContext.Parameters["password"] ?? "null";
+            Console.WriteLine("checking search results and password is " + password);
         }
     }
 }
